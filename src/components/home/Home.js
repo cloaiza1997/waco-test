df388a57ac5065111b38340d055c79fdc16f248e
Cristian Loaiza <kla-1997@hotmail.com>
2020-07-05 11:51:47 -0500

    Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,15 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { useGlobal } from 'reactn';
-
 import { useHistory } from "react-router-dom";
-
+// Class
+import Dashboard from "./views/Dashboard";
+import UserCreate from "./views/UserCreate";
+import UserList from "./views/UserList";
+import Profile from "./views/Profile";
+// Components
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-
 import Button from "@material-ui/core/Button";
-
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -17,15 +18,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+// Icons
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import PowerSettingsNewTwoToneIcon from '@material-ui/icons/PowerSettingsNewTwoTone';
+import PersonAddTwoToneIcon from '@material-ui/icons/PersonAddTwoTone';
+import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone';
+import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone'; 
 
 const drawerWidth = 240;
 
@@ -92,6 +95,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Home() {
+
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -114,6 +118,34 @@ export default function Home() {
     history.push("/");
   }
 
+  const [view, setView] = useState();
+
+  const changeView = (view = "") => {
+
+    let component_view = "";
+
+    switch(view) {
+      case "create":
+        component_view = <UserCreate/>
+      break;
+      case "list":
+        component_view = <UserList/>
+      break;
+      case "profile":
+        component_view = <Profile/>
+      break;
+      default:
+        component_view = <Dashboard/>
+      break;
+    }
+
+    setView(component_view);
+  }
+
+  useEffect(() => {
+    changeView();
+  });
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -132,6 +164,7 @@ export default function Home() {
             className={clsx(classes.menuButton, {
               [classes.hide]: open,
             })}
+            title="Abrir Menú"
           >
             <MenuIcon />
           </IconButton>
@@ -139,13 +172,11 @@ export default function Home() {
             <Typography variant="h6" noWrap>
               Gestión de Usuarios
             </Typography>
-
-            <Button className="logout" onClick={logout}>
+            <Button className="logout" onClick={logout} title="Cerrar Sesión">
               <PowerSettingsNewTwoToneIcon/>
             </Button>
           </div>
         </Toolbar>
-        
       </AppBar>
       <Drawer
         variant="permanent"
@@ -161,38 +192,47 @@ export default function Home() {
         }}
       >
         <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
+          <div className="div-logo">
+            <a href="https://wacoservices.com/" target="_blank" className="o-7x" title="Ir a la página de Waco Services">
+              <img className="w-50x" src={ require("../../assets/img/logo-small.png") } alt="Waco Services"/>
+            </a>
+            <span className="p-5x"></span>
+            <h5 className="m-0x">Waco Services Test</h5>  
+          </div>
+          <IconButton onClick={handleDrawerClose} title="Minimizar Menú">
             {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
         </div>
         <Divider />
         <List>
-          <ListItem button>
+          <ListItem button title="Mi Perfil" onClick={() => changeView("profile")}>
               <ListItemIcon>
                 <img className="nav-user-img" src={globalState.user.photoURL} alt={globalState.user.displayName}/>
               </ListItemIcon>
               <ListItemText primary={globalState.user.displayName} />
           </ListItem>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            <ListItem button title="Ir al Inicio" onClick={() => changeView()}>
+              <ListItemIcon><HomeTwoToneIcon/></ListItemIcon>
+              <ListItemText primary="Inicio" />
             </ListItem>
-          ))}
+            <ListItem button title="Crear un Usuario" onClick={() => changeView("create")}>
+              <ListItemIcon><PersonAddTwoToneIcon/></ListItemIcon>
+              <ListItemText primary="Crear Usuario" />
+            </ListItem>
+            <ListItem button title="Ver Listado de Usuario" onClick={() => changeView("list")}>
+              <ListItemIcon><PeopleAltTwoToneIcon/></ListItemIcon>
+              <ListItemText primary="Listar Usuarios" />
+            </ListItem>
         </List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-       
+        
+        { view }
+
       </main>
     </div>
   );
